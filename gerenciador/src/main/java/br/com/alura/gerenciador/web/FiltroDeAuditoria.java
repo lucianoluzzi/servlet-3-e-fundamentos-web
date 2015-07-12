9274e13a797bd69d26ceeb6be9180be7f3729e1b
Luciano Luzzi <lucianoluzzi@hotmail.com>
2015-07-12 00:48:16 -0300

    alteração no filtro de auditoria

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -1,5 +1,33 @@
 package br.com.alura.gerenciador.web;
 
-public class FiltroDeAuditoria {
+import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter(urlPatterns="/*")
+public class FiltroDeAuditoria implements Filter {
+
+	@Override
+	public void destroy() {	
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {	
+		HttpServletRequest servletRequest = (HttpServletRequest) request;
+		String requestedURI = servletRequest.getRequestURI();
+		System.out.println("Usuario acessando a URI " + requestedURI);
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+	}
 }
